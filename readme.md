0ebe5bcc08e031783f85e1c4f1da3b411d5b4082
Wiktor Kościelny <wiktorkoscielny@gmail.com>
2022-03-28 11:30:58 +0200

    add readme

---
+++ readme.md
@@ -0,0 +1,3 @@
+Branches: master, baasics
+
+It's gonna be state with registered users, and field form to add new user.
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
+
+import React, { useState } from 'react';
+
 
 
 function App() {
+
+  const [people, setPeople] = useState([
+  {
+    name: "Jan Kowalski",
+    url: "",
+    age: 36,
+    note: "experienced user"
+  }, 
+  {
+    name: "Bogdan Nowak",
+    url: "",
+    age: 22,
+    note: "new user"
+  }
+
+]);
+
+people.map(person => {
+  person.
+})
+
   return (
     <div>
-      
+      <h1>Users</h1>
     </div>
   );
 }
